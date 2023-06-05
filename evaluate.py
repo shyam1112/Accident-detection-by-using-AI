2603daf41b64ba7a51829ba4bf284af7fff1f0c6
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-06-05 21:42:38 +0530

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -24,6 +24,7 @@ train_on_gpu = torch.cuda.is_available()
 if not train_on_gpu:
     print('CUDA is not available.  Training on CPU ...')
 
+    
 else:
     print('CUDA is available!  Training on GPU ...')
 ImageFile.LOAD_TRUNCATED_IMAGES = True
